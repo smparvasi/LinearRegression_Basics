16ec40196462b7e6d5aae32e589db12e719c946f
smparvasi <parvas.mamad@gmail.com>
2021-03-01 14:23:05 -0600

    Python_Code_Modification

---
+++ model_LR.py
@@ -0,0 +1,11 @@
+# Linear Regression - The simplest Possible Form
+
+# Import Modules
+!pip install seaborn
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
