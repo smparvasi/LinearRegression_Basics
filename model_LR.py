f18cc591189ece25fe6d97f90cbc69f66908f758
smparvasi <parvas.mamad@gmail.com>
2021-03-01 22:34:30 -0600

    Main_UploadFiles

--- model_LR.py
+++ model_LR.py
@@ -1,11 +1,24 @@
+#%%
 # Linear Regression - The simplest Possible Form
 
-# Import Modules
-!pip install seaborn
+### Part I - Importing Modules and Dataset
+# import pandas as pd
+# import numpy as np
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import tensorflow as tf
 
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-import seaborn as sns
+# # read data
+# df = pd.read_csv('./Dataset/salary.csv')
+# df.head()
+
 import matplotlib.pyplot as plt
+import numpy as np
 
+x = np.linspace(0, 5*np.pi, 1000)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.show()
+
+#%%
